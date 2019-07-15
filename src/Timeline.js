9f1ed581b21e024bff79f1ec9c93ec5dfda2c138
Yukichka <yukiinsw@gmail.com>
2019-07-15 23:10:02 +0100

    Changed display on timeline

--- src/Timeline.js
+++ src/Timeline.js
@@ -3,6 +3,8 @@ import { render } from "react-dom";
 import { travel } from "./dataset";
 import { Country } from "./Country";
 
+import "./timelineStyle.css";
+
 export class Timeline extends React.Component {
   prepareDates() {
     var dates = [];
@@ -10,7 +12,11 @@ export class Timeline extends React.Component {
       country.cities.forEach(city => {
         city.dates.forEach(date => {
           if (date.length) {
-            dates.push({ city: city, date: date, timestamp: this.stringDateToTimestamp(date) });
+            dates.push({
+              city: city,
+              date: date,
+              timestamp: this.stringDateToTimestamp(date)
+            });
           }
         });
       });
@@ -20,21 +26,41 @@ export class Timeline extends React.Component {
   }
 
   stringDateToTimestamp(stringDate) {
-    var arr = stringDate.split(".")
-    return new Date (arr[2], arr[1] - 1, arr[0]).getTime()
+    var arr = stringDate.split(".");
+    return new Date(arr[2], arr[1] - 1, arr[0]).getTime();
   }
 
   render() {
     const citiesByDate = this.prepareDates();
-    citiesByDate.sort(function(a, b){return b.timestamp - a.timestamp});;
-    
+    citiesByDate.sort(function(a, b) {
+      return b.timestamp - a.timestamp;
+    });
+
     return (
-      <div>
-        {citiesByDate.map((el, idx) =>
-          <li key={idx}>
-            {el.city.name} {el.date} 
-          </li>
-        )}
+      <div className="container">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="main-timeline">
+                {citiesByDate.map((el, idx) => {
+                  return (
+                    <div className="timeline" key={idx}>
+                      <div className="icon" />
+                      <div className="date-content">
+                        <div className="date-outer">
+                          <span className="date">
+                            <span className="month">{el.date}</span>
+                            <span className="year">{el.city.name}</span>
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
