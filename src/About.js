0c1831c4868ed675a205d89d9335873199422cae
Yukichka <yukiinsw@gmail.com>
2019-07-16 20:00:23 +0100

    Add icon on About page

--- src/About.js
+++ src/About.js
@@ -1,22 +1,32 @@
 import React from "react";
-import { displayPartsToString } from "typescript";
+import { SocialIcon } from "react-social-icons";
 
 export function About() {
   return (
     <div>
       <h3>About</h3>
       <div className="comment">
-      <p>I am a self-taught junior JavaScript developer.</p>
-      <p>Located in London.</p>
-      <br />
-      
-        <p>Contact me at: 
-        <a href="https://www.linkedin.com/in/yuki-inasawa-23175516a/"> LinkedIn </a>
-        <a href="https://github.com/Yukichka">Github </a>
-        <a href="https://www.instagram.com/ukinnn/">Instagram</a>
+        <p>I am a self-taught junior JavaScript developer.</p>
+        <p>Located in London.</p>
+        <br />
+
+        <p>
+          Contact me at 
+          <SocialIcon
+            url="https://www.linkedin.com/in/yuki-inasawa-23175516a/"
+            style={{ margin: "8px" }}
+          />
+          <SocialIcon
+            url="https://github.com/Yukichka"
+            style={{ margin: "8px" }}
+          />
+          <SocialIcon
+            url="https://www.instagram.com/ukinnn/"
+            style={{ margin: "8px" }}
+          />
         </p>
       </div>
-      <br/>
+      <br />
     </div>
   );
 }
