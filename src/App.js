699281a3742cdb878b81acdcc9c35ca6459721ff
Yukichka <yukiinsw@gmail.com>
2019-07-17 23:13:23 +0100

    Navebar hamburged

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
   Nav,
   NavItem,
   NavLink,
+  NavbarToggler,
   Col
 } from "reactstrap";
 import { NavLink as RRNavLink } from "react-router-dom";
@@ -21,6 +22,21 @@ import { About } from "./About";
 import { AutocompleteCity } from "./AutocompleteCity";
 
 export class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.state = {
+      collapsed: true
+    };
+  }
+
+  toggleNavbar() {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  }
+    
+  
   render() {
     return (
       <Router>
@@ -30,7 +46,8 @@ export class App extends React.Component {
             <Col sm="12" md={{ size: 8, offset: 2 }}>
               <Navbar color="white" light expand="md">
                 <NavbarBrand>Travel & Cafe</NavbarBrand>
-                <Collapse isOpen={true} navbar>
+                <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+                <Collapse isOpen={!this.state.collapsed} navbar>
                   <Nav className="ml-auto" navbar>
                     <NavItem>
                       <NavLink tag={RRNavLink} to="/">
